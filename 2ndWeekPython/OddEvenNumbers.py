d3fc95179c70ac74dd75e12b7764038d9c8ae258
Alireza Amiri <amiri.datascientist@gmail.com>
2024-06-12 23:19:01 +0330

    oddEven

---
+++ 2ndWeekPython/OddEvenNumbers.py
@@ -0,0 +1,13 @@
+x = 63
+
+if x % 65 == 0:
+    print(x, "Is Even Number")
+else:
+    print(x, "Is Odd Number")
+
+y = 12
+
+if y % 12 == 0:
+    print(y, "Is Even Number")
+else:
+    print(y, "Is Odd Number")
